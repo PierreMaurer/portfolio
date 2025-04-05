2f956066d84c0718da5335ce9cd217152d620c3a
Pierre Maurer <91548935+PierreMaurer@users.noreply.github.com>
2025-04-04 23:43:35 -0700

    feat: font in tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,65 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import {Navbar} from "@/components/Navbar";
 import React from "react";
+
+const satoshi = localFont({
+  src: [
+    {
+      path: '../public/fonts/Satoshi-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi-Italic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi-LightItalic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi-MediumItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi-BoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi-Black.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi-BlackItalic.otf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-satoshi',
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +84,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} ${satoshi.variable} antialiased`}
       >
       <Navbar/>
         {children}
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,11 +4,11 @@ export const Hero = () => {
   return (
       <div className="ms-36 mt-36">
           <div className="text-[40px] w-4/5">
-              <p>I am a fullstack web and mobile developer specializing in turning your ambitions into reality.</p>
+              <span className="font-satoshi-light">I am a</span> <span className="font-satoshi-medium">fullstack web and mobile</span><span>developer specializing in turning your</span> <span>ambitions</span> <span>into</span> <span>reality.</span>
           </div>
           <div className="mt-22">
               <p className="text-[126px]">Fullstack Developer</p>
-              <p className="text-6xl opacity-65 font-light">PIERRE MAURER</p>
+              <p className="text-6xl opacity-65 font-satoshi-light">PIERRE MAURER</p>
           </div>
           <div className="flex flex-row justify-between me-36 mt-52">
               <div className="flex flex-row gap-3">
