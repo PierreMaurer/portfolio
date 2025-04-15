1160509ea4c561d7abf074ee8ad2c3271f798843
Pierre Maurer <91548935+PierreMaurer@users.noreply.github.com>
2025-04-15 10:14:38 -0700

    feat: responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,13 @@
 export const Navbar = () => {
   return (
-      <div className="flex flex-row justify-between ms-36 me-36 h-16 items-center">
+      <div className="flex flex-row justify-between ms-5 me-5 lg:ms-36 lg:me-36 h-16 items-center">
           <div>
-              <p>PIERRE MAURER</p>
+              <p className="text-sm">PIERRE MAURER</p>
           </div>
-          <div className="flex flex-row gap-24">
+          <div className="flex flex-row gap-5 lg:gap-24 text-sm">
               <a href='#project'><p>PROJECTS</p></a>
               <a href='#profil'><p>ABOUT ME</p></a>
               <p>CONTACT ME</p>
-          </div>
-          <div>
-              <p>FR</p>
-              <hr/>
-              <p>EN</p>
           </div>
       </div>
   )
